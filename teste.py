abc7e948bea615ceb6904b8e18f9f075eb7f3166
Bruno Dantas Moreira <68398839+BrunoDantasMoreira@users.noreply.github.com>
2024-11-07 13:56:07 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-string = [r'C:\Users\Dell\Pictures\WallPapers\data\frame1608.jpg', 
-          r'C:\Users\Dell\Pictures\WallPapers\data\frame1609.jpg']
-          
-
-with open('prints.txt', 'a') as txt:
-
-  for c in string:
-    txt.write(c + '\n')
